37e1c089da127a3f394a54fa42a35eacb62a12d3
JuneThreeSitompul <junethree12@gmail.com>
2021-11-16 12:02:17 +0700

    first commit

---
+++ database/seeds/CouriersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Courier;
+class CouriersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            ['code' => 'jne','title' => 'JNE'],
+            ['code' => 'pos','title' => 'POS'],
+        ];
+        Courier::insert($data);
+    }
+}
